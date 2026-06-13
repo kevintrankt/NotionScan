1561a75d0f113d97a894c1c497e30b88355f163b
Kevin Tran <kevin.tran.kt@gmail.com>
2026-06-13 14:32:15 -0700

    Initial Commit

---
+++ NotionScan/NotionScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotionScanApp.swift
+//  NotionScan
+//
+//  Created by keb on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct NotionScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
